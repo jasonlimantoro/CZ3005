4b75764504d291931b89d22b9765de8e00ed236e
jasonlimantoro <jasonlimantoro99@gmail.com>
2019-04-08 21:07:54 +0800

    rename addons to sides, add drinks

--- main.pl
+++ main.pl
@@ -2,7 +2,7 @@
 :- ['module.pl'].
 
 % Declare dynamic predicates to store results
-:- dynamic meal/1, bread/1, main/1, cheese/1, veg/1, sauce/1, cookie/1, addon/1.
+:- dynamic meal/1, bread/1, main/1, cheese/1, veg/1, sauce/1, cookie/1, side/1, drink/1.
 
 /**
  * Interactive validation for each menu
@@ -61,15 +61,23 @@ validate_cookie :-
         true
     ).
 
-validate_addon :-
+validate_side :-
     read(X),
     (not(X == 0) -> 
-        (valid_addon(X) -> write('addon = '), write(X), write(' (Enter 0 to conclude addons)'), nl, assert(addon(X));
-        write('Invalid addon, try again'), nl),
-        validate_addon;
+        (valid_side(X) -> write('sides = '), write(X), write(' (Enter 0 to conclude sides)'), nl, assert(side(X));
+        write('Invalid sides, try again'), nl),
+        validate_side;
         true
     ).
 
+validate_drink :-
+    read(X),
+    (not(X == 0) -> 
+        (valid_drink(X) -> write('drinks = '), write(X), write(' (Enter 0 to conclude drinks)'), nl, assert(drink(X));
+        write('Invalid drinks, try again'), nl),
+        validate_drink;
+        true
+    ).
 /**
  * Interactive prompt for each menu
  */
@@ -99,7 +107,7 @@ ask_sauce :-
     validate_sauce.
 
 ask_healthy_sauce :-
-    write('Please choose healthy sauces: '),nl,
+    write('Please choose healthy sauce: '),nl,
     display_choices(healthy_sauces),
     validate_healthy_sauce.
 
@@ -108,35 +116,40 @@ ask_cookie :-
     display_choices(cookies),
     validate_cookie.
 
-ask_addon :-
-    write('Please choose addon: '),nl,
-    display_choices(addons),
-    validate_addon.
+ask_side :-
+    write('Please choose sides: '),nl,
+    display_choices(sides),
+    validate_side.
+
+ask_drink :-
+    write('Please choose drinks: '),nl,
+    display_choices(drinks),
+    validate_drink.
 
 /**
  * Main flow given the meal type
  */
 meal_normal :-
     ask_bread, ask_main, ask_cheese, ask_veg,
-    ask_sauce, ask_cookie, ask_addon.
+    ask_sauce, ask_cookie, ask_side, ask_drink.
 
 meal_vegan :-
     ask_bread, ask_veg,
-    ask_healthy_sauce, ask_addon.
+    ask_healthy_sauce, ask_side, ask_drink.
 
 meal_veggie :-
     ask_bread, ask_veg,
-    ask_healthy_sauce, ask_cookie, ask_addon.
+    ask_healthy_sauce, ask_cookie, ask_side, ask_drink.
 
 meal_value :-
     ask_bread, ask_main, ask_cheese,
-    ask_veg, ask_sauce.
+    ask_veg, ask_sauce, ask_drink.
 
 
 /**
  * Gathering all user inputs 
 */
-get_choices(Meals, Breads, Mains, Cheeses, Vegs, Sauces, Cookies, Addons) :-
+get_choices(Meals, Breads, Mains, Cheeses, Vegs, Sauces, Cookies, Sides, Drinks) :-
     findall(X, meal(X), Meals),
     findall(X, bread(X), Breads),
     findall(X, main(X), Mains),
@@ -144,13 +157,14 @@ get_choices(Meals, Breads, Mains, Cheeses, Vegs, Sauces, Cookies, Addons) :-
     findall(X, veg(X), Vegs),
     findall(X, sauce(X), Sauces),
     findall(X, cookie(X), Cookies),
-    findall(X, addon(X), Addons).
+    findall(X, side(X), Sides),
+    findall(X, drink(X), Drinks).
 
 /**
  * Display them prettily
  */ 
 overview :-
-    get_choices(Meals, Breads, Mains, Cheeses, Vegs, Sauces, Cookies, Addons), 
+    get_choices(Meals, Breads, Mains, Cheeses, Vegs, Sauces, Cookies, Sides, Drinks), 
     atomic_list_concat(Meals, Meal),
     write('Selected Meal: '), write(Meal), nl,
 
@@ -169,8 +183,10 @@ overview :-
     write('Sauces: '), write(Sauce), nl,
     atomic_list_concat(Cookies, ',', Cookie),
     write('Cookies: '), write(Cookie), nl,
-    atomic_list_concat(Addons, ',', Addon),
-    write('Addons: '), write(Addon), nl.
+    atomic_list_concat(Sides, ',', Side),
+    write('Sides: '), write(Side), nl,
+    atomic_list_concat(Drinks, ',', Drink),
+    write('Drinks: '), write(Drink), nl.
 
 
 /**
@@ -183,7 +199,7 @@ flush :- retract(cheese(_)), fail.
 flush :- retract(veg(_)), fail.
 flush :- retract(sauce(_)), fail.
 flush :- retract(cookie(_)), fail.
-flush :- retract(addon(_)), fail.
+flush :- retract(side(_)), fail.
 
 start:-
     write('--------------------END SUBWAY--------------------'), nl,
--- module.pl
+++ module.pl
@@ -15,7 +15,8 @@ display_choices(vegs):- vegs(L), write('vegs = '), choices(L), nl.
 display_choices(sauces):- sauces(L), write('sauces = '), choices(L), nl.
 display_choices(healthy_sauces):- healthy_sauces(L), write('sauces = '), choices(L), nl.
 display_choices(cookies):- cookies(L), write('cookies = '), choices(L), nl.
-display_choices(addons):- addons(L), write('addons = '), choices(L), nl.
+display_choices(sides):- sides(L), write('sides = '), choices(L), nl.
+display_choices(drinks):- drinks(L), write('drinks = '), choices(L), nl.
 
 
 /*
@@ -28,7 +29,8 @@ valid_veg(X) :- vegs(V), member(X, V), !.
 valid_sauce(X) :- sauces(S), member(X, S), !.
 valid_healthy_sauce(X) :- healthy_sauces(S), member(X, S), !.
 valid_cookie(X) :- cookies(C), member(X, C), !.
-valid_addon(X) :- addons(A), member(X, A), !.
+valid_side(X) :- sides(A), member(X, A), !.
+valid_drink(X) :- drinks(D), member(X, D), !.
 
 
 /*
@@ -41,4 +43,5 @@ vegs([lettuce, tomato, cucumber, capsicum, onion, jalapeno, pickle, avocado]).
 sauces([bbq, chili, honeymustard, mustard, sweetonion, redwine, mayonnaise, chipotle, ranch, vinegar]).
 healthy_sauces([redwine, chipotle, ranch, vinegar]).
 cookies([chocochip, doublechocochip, oatmeal, white_choco_chip, macnut, raspberry, brownie]).
-addons([drink, soup, chips]).
+sides([chips, yoghurts, cookies, energy_bar]).
+drinks([dasani, fountain, pulpy, green_tea, coffee, tea]).
